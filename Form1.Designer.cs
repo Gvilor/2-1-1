439d7d5471cab3832af2f4b4d07cb7df4e880094
Gvilor <gvilor@yandex.ru>
2021-10-20 14:35:03 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace lab_2_1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtn1 = new System.Windows.Forms.TextBox();
+            this.txtn3 = new System.Windows.Forms.TextBox();
+            this.txtn2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtn1
+            // 
+            this.txtn1.Location = new System.Drawing.Point(12, 12);
+            this.txtn1.Name = "txtn1";
+            this.txtn1.Size = new System.Drawing.Size(100, 23);
+            this.txtn1.TabIndex = 1;
+            // 
+            // txtn3
+            // 
+            this.txtn3.Location = new System.Drawing.Point(12, 70);
+            this.txtn3.Name = "txtn3";
+            this.txtn3.Size = new System.Drawing.Size(100, 23);
+            this.txtn3.TabIndex = 2;
+            // 
+            // txtn2
+            // 
+            this.txtn2.Location = new System.Drawing.Point(12, 41);
+            this.txtn2.Name = "txtn2";
+            this.txtn2.Size = new System.Drawing.Size(100, 23);
+            this.txtn2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "n1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "n2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "n3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(166, 133);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtn2);
+            this.Controls.Add(this.txtn3);
+            this.Controls.Add(this.txtn1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtn1;
+        private System.Windows.Forms.TextBox txtn3;
+        private System.Windows.Forms.TextBox txtn2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_2_1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtn1.Text = Properties.Settings.Default.n1.ToString();
+            txtn2.Text = Properties.Settings.Default.n2.ToString();
+            txtn3.Text = Properties.Settings.Default.n3.ToString();
+        }
+        public class Logic
+        {
+
+            public static int Multiplication(int n1, int n2, int n3)
+            {
+
+
+                int otvet = 0;
+
+                if (n1 >= n2 & n1 >= n3)
+                {
+                    otvet = n2 * n3;
+                }
+                else if (n2 >= n1 & n2 >= n3)
+                {
+                    otvet = n1 * n3;
+                }
+                else
+                {
+                    otvet = n2 * n1;
+                }
+
+                return otvet;
+            }
+
+        }
+        private void txtx_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1, n2, n3;
+
+            try
+            {
+                n1 = int.Parse(this.txtn1.Text);
+                txtn1.BackColor = Color.White;
+            }
+            catch
+            {
+                txtn1.BackColor = Color.Red;
+                MessageBox.Show("Введено некоректное значение", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                n2 = int.Parse(this.txtn2.Text);
+                txtn2.BackColor = Color.White;
+            }
+            catch
+            {
+                txtn2.BackColor = Color.Red;
+                MessageBox.Show("Введено некоректное значение", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                n3 = int.Parse(this.txtn3.Text);
+                txtn3.BackColor = Color.White;
+            }
+            catch
+            {
+                txtn3.BackColor = Color.Red;
+                MessageBox.Show("Введено некоректное значение", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+             
+
+            Properties.Settings.Default.n1 = n1;
+            Properties.Settings.Default.n2 = n2;
+            Properties.Settings.Default.n3 = n3;
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show($"Ответ: {Logic.Multiplication(n1, n2, n3)}");
+        }
+
+    }
+}
